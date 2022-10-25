248b54914c4ec72a8c66948438733996eea7093d
Nazar Maksymchuk <nzrbeats@gmail.com>
2022-10-24 20:41:16 -0700

    lint error

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,14 @@
+/* eslint-disable react/jsx-no-target-blank */
 export default function Footer() {
   return (
     <footer>
-      <div className="max-w-6xl mx-auto">Copyright NatomasNews. 2022</div>
+      <div className="max-w-6xl mx-auto">
+        Copyright NatomasNews. 2022. All rights reserved. A{" "}
+        <a href="https://n87m.com/" target="_blank">
+          N87M
+        </a>{" "}
+        Company.
+      </div>
     </footer>
   );
 }
